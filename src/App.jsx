9793906f6efffb2152e2b211156f0ebdef86d05a
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-03-12 15:46:43 +0100

    Initialize react-weather-app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+    <h1 className='text-rose-500 font-semibold text-xl'>Welcome to my App</h1>
+     
+    </>
+  )
+}
+
+export default App
