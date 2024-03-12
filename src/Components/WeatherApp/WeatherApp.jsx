e8bed18c11fac281685e6b5e4bf182a16d698024
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-03-12 16:28:32 +0100

    re-arrange folders, to successfull imported images

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,4 +1,11 @@
-import search_icn from '../src/assets/img.search.png'
+import search_icn from '../WeatherApp/img/search.png'
+import clear_icn from '../WeatherApp/img/clear.png'
+import cloud_icn from '../WeatherApp/img/cloud.png'
+import drizzle_icn from '../WeatherApp/img/drizzle.png'
+import humidity_icn from '../WeatherApp/img/humidity.png'
+import rain_icn from '../WeatherApp/img/rain.png'
+import snow_icn from '../WeatherApp/img/snow.png'
+import wind_icn from '../WeatherApp/img/wind.png'
 
 const WeatherApp = () => {
   return (
