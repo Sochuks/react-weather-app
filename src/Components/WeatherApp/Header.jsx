ad47d8e826fc1fdf3ba2ff8d30cb2410ec60445b
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-03-13 16:03:29 +0100

    validate input box for city & consoleLog

--- src/Components/WeatherApp/Header.jsx
+++ src/Components/WeatherApp/Header.jsx
@@ -1,6 +1,22 @@
+import { useState } from 'react'
 import search_icn from '../WeatherApp/img/search.png'
 
 const Header = () => {
+
+    const [city, setCity] = useState('')
+
+    // Validate form
+    const checkCity = (e) => {
+        e.preventDefault();
+
+        if (!city){
+            alert('Add a country')
+        }else{
+            console.log(city)
+            setCity('')
+        }
+    }
+
   return (
     <header className='pt-10'>
         {/* Header container */}
@@ -8,10 +24,13 @@ const Header = () => {
             
             {/* Search Box */}
             <>
-                <input type="text" placeholder='Search' className="p-4 text-xl font-normal bg-green-100" />
+                <input type="text" placeholder='Search' 
+                className="p-4 text-xl font-normal bg-green-100" 
+                value={city} onChange={(e) => {setCity(e.target.value)}}/>
             </>
             {/* Search Icon */}
-            <div className="flex justify-center bg-green-100 rounded-full w-14 h-14 items-center">
+            <div className="flex justify-center bg-green-100 rounded-full w-14 h-14 items-center"
+            onClick={checkCity}>
                 <img src={search_icn} alt="Search" />
             </div>
         </div>
