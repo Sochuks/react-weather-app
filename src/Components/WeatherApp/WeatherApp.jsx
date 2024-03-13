1d115ac90b893e4b1739a0ac0ec8de120a5eb62d
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-03-13 14:48:09 +0100

    Added api key from openweather

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -2,8 +2,10 @@ import Header from './Header'
 import Weather from './Weather'
 
 const WeatherApp = () => {
+    // api key
+    let api_key = "09f9327956dcd39a294c20990d9d4eb5"
   return (
-    <div className='app-main flex-column mx-auto rounded-md'>
+    <div className='app-main flex-col mx-auto rounded-md'>
         {/* Header Component */}
         <Header />
         {/* Weather Component */}
