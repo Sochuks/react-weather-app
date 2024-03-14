576f08fac32e36d4375dc30e7f7f7bee84c46bac
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-03-14 09:37:42 +0100

    modified syntax to set state correctly

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -6,7 +6,9 @@ import Weather from './Weather'
 
 const WeatherApp = () => {
 // Declare useState
-const [weatherData, setWeatherData] = {}
+const [weatherData, setWeatherData] = useState()
+
+
   return (
     <div className='app-main flex-col mx-auto rounded-md'>
         {/* Header Component */}
