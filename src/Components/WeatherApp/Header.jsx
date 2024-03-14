5d691a3c689086f072a255adb306ad62bb46e363
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-03-14 10:22:33 +0100

    modify code syntax from errors and call addCity

--- src/Components/WeatherApp/Header.jsx
+++ src/Components/WeatherApp/Header.jsx
@@ -1,20 +1,21 @@
 import { useState } from 'react'
 import search_icn from '../WeatherApp/img/search.png'
 
-const Header = (addCity) => {
+const Header = ({addCity}) => {
 
-    const [city, setCity] = useState('')
+    const [cityInput, setCityInput] = useState('')
 
     // Validate form
     const checkCity = (e) => {
         e.preventDefault();
 
-        if (!city){
-            alert('Add a country')
-        }else{
-            console.log(city)
-            setCity('')
+        if (!cityInput.trim()){
+            alert('Enter city name')
+            return
         }
+        // call addCity prop
+        addCity(cityInput.trim())
+        setCity('')
     }
 
   return (
@@ -26,7 +27,7 @@ const Header = (addCity) => {
             <>
                 <input type="text" placeholder='Search' 
                 className="p-4 text-xl font-normal bg-green-100" 
-                value={city} onChange={(e) => {setCity(e.target.value)}}/>
+                value={cityInput} onChange={(e) => {setCityInput(e.target.value)}}/>
             </>
             {/* Search Icon */}
             <div className="flex justify-center bg-green-100 rounded-full w-14 h-14 items-center"
